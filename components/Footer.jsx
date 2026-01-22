c41b5180cda295fb03c209ff07d66afcced97a72
unkfex-rgb <unkfex@gmail.com>
2026-01-21 23:40:04 -0300

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,17 +1,10 @@
 
-"use client";
-
 export default function Footer() {
   return (
     <footer>
-      <span
-        className="dev"
-        onClick={() =>
-          window.open("https://www.instagram.com/corintia420/", "_blank")
-        }
-      >
+      <div className="footer-dev" onClick={() => window.open('https://www.instagram.com/corintia420/', '_blank')}>
         Dev by Corintia420
-      </span>
+      </div>
     </footer>
   );
 }
