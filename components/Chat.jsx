5404bbcaa515d79346b16e037bfce75deeea30c7
unkfex-rgb <unkfex@gmail.com>
2026-01-21 23:32:37 -0300

    Create Chat.jsx

---
+++ components/Chat.jsx
@@ -0,0 +1,14 @@
+
+"use client";
+
+export default function Chat({ streamer }) {
+  return (
+    <iframe
+      src={`https://www.twitch.tv/embed/${streamer}/chat?parent=loud-multi.vercel.app`}
+      width="100%"
+      height="100%"
+      frameBorder="0"
+      scrolling="no"
+    ></iframe>
+  );
+}
